4575c40b261e2f5f060c3b7a2445f3c499dbf479
Lily <alinecm@usp.br>
2026-01-06 18:54:14 -0300

    Kojima

---
+++ structure.py
@@ -0,0 +1,27 @@
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import QSize
+
+
+class ImageButtonExample(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PNG Button")
+        self.resize(300, 200)
+
+        layout = QVBoxLayout(self)
+
+        button = QPushButton()
+        button.setText("")
+        button.setIcon(QIcon("Kojima.png"))  # path to your PNG
+        button.setIconSize(QSize(600, 600))  # size of the image
+        button.setFixedSize(800, 800)        # size of the button
+
+        layout.addWidget(button)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ImageButtonExample()
+    window.show()
+    sys.exit(app.exec())
