d4f82d30b6f34a4fb37e023e57bd0f40caf76633
Lily <alinecm@usp.br>
2026-01-11 15:55:27 -0300

    rolling advantage

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,6 @@
 import random
 
-def dice(n: int) -> int:
+def dice(n: int, advantage: int) -> int:
     if n < 1:
         raise ValueError("n must be >= 1")
-    return random.randint(1, n)
+    return max(random.randint(1, n) for _ in range(advantage))
