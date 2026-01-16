07a5eae1a96d5a90d0979f00c6d218b6f5e054a1
Lily <alinecm@usp.br>
2026-01-16 16:45:05 -0300

    Pipe test 1: Success

--- src/main.py
+++ src/main.py
@@ -4,17 +4,24 @@ from PySide6.QtWidgets import QApplication
 
 from objects.objects import Personagem
 from interface import Window
+from pipes import Pipe
 
 def handle_vida(personagem, vida):
     personagem.setVida(vida)
     window.setValue(personagem.getVida())
-    
+
+def handle_vida2(personagem, vida):
+    Pipe(
+        lambda p, v: p.setVida(v),
+        lambda p, v: window.setValue(p.getVida()),
+        2
+    ).execute((personagem, vida))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    Personagem1 = Personagem(vida=100, sanidade=100)
+    Personagem1 = Personagem(vida=100, sanidade=100, nivel=10, classe="Classe")
     window = Window()
-    window.increment_clicked.connect(lambda: handle_vida(Personagem1, 25))
+    window.increment_clicked.connect(lambda: handle_vida2(Personagem1, 25))
     
     window.show()
     sys.exit(app.exec())
