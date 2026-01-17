e0d00edccb3cf47d0ce9eebf05fa15e5a66759b8
Lily <alinecm@usp.br>
2026-01-17 17:27:18 -0300

    fix: main window broke

--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QDialog, QLineEdit, QHBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 from PySide6.QtGui import QIcon, QFont, QColor
 from PySide6.QtCore import QSize, Signal
 
@@ -48,57 +48,12 @@ class Window(QWidget):
         self.layout.addWidget(self.btn1)
         self.layout.addWidget(self.label)
         
-        self.btn2 = StyledButton(200, 60, "Set Value", "#4a7abc")
-        self.layout.addWidget(self.btn2)
-
-        self.btn2.clicked.connect(self.open_dialog)
         self.btn1.clicked.connect(self.increment_clicked)
         
     def setValue(self, value):
         self.label.setText(f"Value: {value}")
-    
-    def open_dialog(self):
-        dialog = ValueDialog(self)
-
-        if dialog.exec():
-            text = dialog.value()
-
-            if text.isdigit():
-                self.setValue(int(text))
-
         
         
-        
-
-class ValueDialog(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle("Insert value")
-        self.setFixedSize(300, 120)
-
-        layout = QVBoxLayout(self)
-
-        self.input = QLineEdit()
-        self.input.setPlaceholderText("Enter a number")
-
-        buttons_layout = QHBoxLayout()
-        ok_btn = QPushButton("OK")
-        cancel_btn = QPushButton("Cancel")
-
-        ok_btn.clicked.connect(self.accept)
-        cancel_btn.clicked.connect(self.reject)
-
-        buttons_layout.addWidget(ok_btn)
-        buttons_layout.addWidget(cancel_btn)
-
-        layout.addWidget(QLabel("Value:"))
-        layout.addWidget(self.input)
-        layout.addLayout(buttons_layout)
-
-    def value(self):
-        return self.input.text()
-    
-    
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
