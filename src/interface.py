eebe9858d10aa45b493ba3bcc862d7f281561659
Lily <alinecm@usp.br>
2026-01-22 20:46:44 -0300

    interface fix 0 esforço + fontes e fontesize

--- src/interface.py
+++ src/interface.py
@@ -1,7 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit
 from PySide6.QtGui import QIcon, QFont, QColor
-from PySide6.QtCore import QSize, Signal
+from PySide6.QtCore import QSize, Signal, Qt
 from functions import Wrapper
 
 class StyledButton(QPushButton):
@@ -43,7 +43,9 @@ class Window(QWidget):
     def __init__(self, vida, sanidade, esforco):
         super().__init__()
         self.setWindowTitle("Darius 0.1")
-        self.resize(400, 200)
+        self.resize(400, 300)
+        
+        self.font = QFont("Times", 25)
         
         self.total = QVBoxLayout(self)
         
@@ -54,20 +56,23 @@ class Window(QWidget):
         
         
         self.label_vida = QLabel(f"Vida: {vida}")
-        self.container1.addWidget(self.label_vida)
+        self.label_vida.setFont(self.font)
         
         self.input_vida = QLineEdit()
         self.input_vida.setPlaceholderText("...")
         self.input_vida.setFixedWidth(200)
-        self.container1.addWidget(self.input_vida)
+        
+        self.container1.addWidget(self.label_vida, alignment=Qt.AlignCenter | Qt.AlignCenter)
+        self.container1.addWidget(self.input_vida, alignment=Qt.AlignCenter | Qt.AlignCenter)
         
         self.label_sanidade = QLabel(f"Sanidade: {sanidade}")
-        self.container2.addWidget(self.label_sanidade)
-        
+        self.label_sanidade.setFont(self.font)
         self.input_sanidade = QLineEdit()
         self.input_sanidade.setPlaceholderText("...")
         self.input_sanidade.setFixedWidth(200)
-        self.container2.addWidget(self.input_sanidade)
+        
+        self.container2.addWidget(self.label_sanidade, alignment=Qt.AlignCenter | Qt.AlignCenter)
+        self.container2.addWidget(self.input_sanidade, alignment=Qt.AlignCenter | Qt.AlignCenter)
         
         self.bar.addLayout(self.container1)
         self.bar.addLayout(self.container2)
@@ -77,6 +82,7 @@ class Window(QWidget):
         self.input_sanidade.returnPressed.connect(self.set_sanidade)
         
         self.label_esforco = QLabel(f"Esforço: {esforco}")
+        self.label_esforco.setFont(self.font)
         self.total.addWidget(self.label_esforco)
         
         self.esforco_deduct = StyledButton(200, 60, "Esforço Gastar", "#cc5632")
@@ -97,7 +103,8 @@ class Window(QWidget):
     
     def deductValue(self, label: QLabel):
         current = int(label.text().split(':')[1].strip())
-        label.setText(f"{label.text().split(':')[0]}: {current - 1}")
+        if current > 0:
+            label.setText(f"{label.text().split(':')[0]}: {current - 1}")
 
 
         
